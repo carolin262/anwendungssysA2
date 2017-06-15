919bc48d4a28f2edd276bc1f0de22860a9cf7ca7
jiafug <comet.liu@protonmail.com>
2017-06-15 20:30:26 +0200

    Servlet der Detailansicht erstellt

---
+++ SmartMeterManagementWeb/src/de/tub/as/smm/DetailServlet.java
@@ -0,0 +1,37 @@
+package de.tub.as.smm;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.tub.as.smm.dao.SmartMeterDao;
+import de.tub.as.smm.models.SmartMeter;
+
+/**
+ * Servlet implementation class SmartMeterServlet
+ */
+@WebServlet("/detail")
+public class DetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// Injected DAO EJB:
+	@EJB
+	SmartMeterDao smartmeterDao;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		for (SmartMeter smartmeter : smartmeterDao.getAllSmartMeters()) {
+			if (Long.parseLong(request.getParameter("id")) == smartmeter.id)
+				request.setAttribute("smartmeter", smartmeter);
+		}
+		request.getRequestDispatcher("/detail.jsp").forward(request, response);
+	}
+
+}
