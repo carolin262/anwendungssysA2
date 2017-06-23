7ae1348f83385be5e30a4ffdc973ee470d575af5
jiafug <comet.liu@protonmail.com>
2017-06-23 20:07:32 +0200

    nicht mehr benötigte Methoden entfernt

--- SmartMeterManagementEJB/ejbModule/de/tub/as/smm/dao/UserDao.java
+++ SmartMeterManagementEJB/ejbModule/de/tub/as/smm/dao/UserDao.java
@@ -30,31 +30,12 @@ public class UserDao {
 		TypedQuery<User> query = em.createQuery("SELECT u FROM User u ORDER BY u.id", User.class);
 		return query.getResultList();
 	}
-
-	// retrieve currently logged in user
-	public List<User> getLoggedInUser() {
-		TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.status = TRUE", User.class)
-				.setMaxResults(1);
-		return query.getResultList();
-	}
 	
 	// find a user 
 	public List<User> findUserByName(String name) {
 		TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.name LIKE :userName", User.class)
 				.setMaxResults(1).setParameter("userName", name);
 		return query.getResultList();
-	}
-
-	// set logged in status from true to false
-	public void logoutUser(Long userId) {
-		em.createQuery("UPDATE User SET status = FALSE WHERE id LIKE :userId")
-				.setParameter("userId", userId).executeUpdate();
-	}
-	
-	// set logged in status from false to true
-	public void logInUser(Long userId) {
-		em.createQuery("UPDATE User SET status = TRUE WHERE id LIKE :userId")
-				.setParameter("userId", userId).executeUpdate();
 	}
 	
 	// update last login date
